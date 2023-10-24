120f618dfc46442749df5f5a04efae0cdc94f837
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2023-10-24 00:36:34 -0400

    switching to a micro with SSD1306

---
+++ ecu_interface_v2/ecu_interface_v2.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3D 
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("version")
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
