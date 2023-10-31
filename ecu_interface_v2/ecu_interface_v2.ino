dd65910eb02119293d3f960d2c9f1bc00e1f4a45
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2023-10-31 01:48:41 -0400

    Gear bitmaps working
    Gear bitmaps displaying correctly. Flash use for bitmaps at an acceptable level.

--- ecu_interface_v2/ecu_interface_v2.ino
+++ ecu_interface_v2/ecu_interface_v2.ino
@@ -4,14 +4,14 @@
 #include "images.h"
 
 //timer for interrupts
-#define SCHED_ROLLOVER 1000 //timer rollover in milliseconds
+#define SCHED_ROLLOVER 300 //timer rollover in milliseconds
 volatile uint8_t task_sched = 0;
 volatile uint16_t timer_ms = 0; //actually 1 timer period, not 1ms
-void timerSetup(uint16_t period){
+void timerSetup(uint16_t period){//period roughly in MS
   TCCR1A = 0; //no OC
   TCCR1B = 0; //clear control register
   TCCR1B = (1<<CS12)|(0<<CS11)|(1<<CS10)|(0<<WGM13)|(1<<WGM12); //1024 prescaler, counting up to OCR1A
-  OCR1A = 160; //16,000,000/1024/ICR1 = timer frequency, roughly 16/ms  
+  OCR1A = 16*period; //16,000,000/1024/ICR1 = timer frequency
   TIMSK1 |= (1<<OCIE1A);
 }
 ISR(TIMER1_COMPA_vect){ //timer interrupt handler
@@ -36,6 +36,9 @@ ISR(TIMER1_COMPA_vect){ //timer interrupt handler
 #define LLINE_PIN 2 //any GPIO
 char init_sequence[] = {0xC1, 0x33, 0xF1, 0x81, 0x66};
 
+//misc
+uint8_t gear = 0;
+
 
 
 
@@ -47,7 +50,7 @@ void setup() {
   Serial.println("Motorcycle diagnostic interface");
 
   //timer setup
-  timerSetup(1);
+  timerSetup(10);
 
 
   //display setup
@@ -61,8 +64,6 @@ void setup() {
   display.print(F("SUZUKI"));
   display.display();
   delay(3000);
-  // display.clearDisplay();
-  // display.display();
 
   //diag UART setup
   pinMode(KLINE_PIN, OUTPUT);//setup to send init on k and l lines
@@ -71,17 +72,34 @@ void setup() {
   Serial1.begin(BAUDRATE);
   Serial1.write(init_sequence, 5);
 
-  
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(task_sched){
-    display.clearDisplay();
-    display.setCursor(15,20); //find center later
-    display.print(timer_ms);
-    Serial.println(timer_ms);
+    if(timer_ms > 200){
+      gear = 2;
+    }
+    else if(timer_ms > 100){
+      gear = 1;
+    }
+    else{
+      gear = 0;
+    }
+    switch(gear){//display current gear
+      case 0:
+        display.clearDisplay();
+        display.drawBitmap(0,0,neutral,32,64,WHITE);
+        break;
+      case 1:
+        display.clearDisplay();
+        display.drawBitmap(0,0,first,32,64,WHITE);
+        break;
+      default:
+        display.clearDisplay();
+        display.drawBitmap(0,0,uncertain, 32,64,WHITE);
+        break;
+    }
     display.display();
     task_sched = 0;
   }
