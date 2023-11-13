56125444aca3213fe3097b2066a914cd3bd363af
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2023-11-13 02:08:34 -0500

    Updated read and parse
    Updated UART read to reflect ECU response timing. Started on parser for stats.

--- serial_sender/serial_sender.ino
+++ serial_sender/serial_sender.ino
@@ -8,7 +8,8 @@
 volatile uint8_t task_sched = 0;
 volatile uint8_t send_keepalive = 1;
 volatile uint16_t timer_ms = 0; //actually 1 timer period, not 1ms
-void timerSetup(uint16_t period){//period roughly in MS
+void timerSetup(uint16_t period){//period roughly in MS with a 16MHz clock. 
+  //Note that if you are not using a 32u4-based MCU, you will have to change the register names/setup appropriately
   TCCR1A = 0; //no OC
   TCCR1B = 0; //clear control register
   TCCR1B = (1<<CS12)|(0<<CS11)|(1<<CS10)|(0<<WGM13)|(1<<WGM12); //1024 prescaler, counting up to OCR1A
@@ -31,7 +32,9 @@ ISR(TIMER1_COMPA_vect){ //timer interrupt handler
 #define OLED_RESET     -1 
 #define SCREEN_ADDRESS 0x3D 
 #define SSD1306_NO_SPLASH
-uint8_t valid_cmd = 0;
+uint8_t valid_cmd = 0; //for if the user command is valid
+uint8_t line = 0;//for scrolling the display
+
 
 //diag port 
 #define BAUDRATE 10400
@@ -40,18 +43,30 @@ uint8_t valid_cmd = 0;
 uint8_t initialized = 0;
 char write_buf[32]; //buffer for data to be written to ecu
 String cmd_buf; //buffer for commands read in from usb serial
-char data_buf[128]; //buffer for data recieved from ecu
+char data_buf[64]; //buffer for data recieved from ecu
 
 //messages 
-//format: 81 12 F1 (ecu address) <request> <checksum>
+//format: 81 12 F1 (ecu address) <request length> <request> <checksum>
 const static char PROGMEM init_msg[] =    {0x81, 0x12, 0xF1, 0x81, 0x05}; //initialize, 5 bytes
 const static char PROGMEM keepalive[] =   {0x80, 0x12, 0xF1, 0x01, 0x3E, 0xC2}; //keepalive, 6 bytes
 const static char PROGMEM quit[] =        {0x80, 0x12, 0xF1, 0x01, 0x82, 0x06}; //quit communcations, 6 bytes
 const static char PROGMEM clear_dtc[] =   {0x80, 0x12, 0xF1, 0x03, 0x14, 0x00, 0x00, 0x9A}; //clear DTC, 8 bytes
 const static char PROGMEM sensor_data[] = {0x80, 0x12, 0xF1, 0x02, 0x21, 0x08, 0xAE}; //query all sensors, 7 bytes
 
-//misc
-uint8_t line = 0;//for scrolling the display
+//stats 
+uint8_t speed = 0; //speed in KPH, byte 16 * 2
+uint16_t rpm = 0; //RPM, 10*byte 17 + byte 18/10
+uint8_t tps = 0; //throttle position sensor value, byte 19 range 0x37-0xDD
+uint8_t iap1 = 0; //intake air pressure 1, byte 20
+uint8_t engine_temp = 0; //engine oil temperature in celsius, (byte 21-48)/1.6
+uint8_t intake_temp = 0; //intake air temperature in celsius, (byte 22-48)/1.6
+uint8_t v_bat = 0; //battery voltage, byte 24*20/255
+uint8_t gear = 0; //gear, byte 26
+uint8_t iap2 = 0; //intake air pressure 2, byte 27
+uint8_t throt_valv = 0; //throttle valve, byte 46
+uint8_t clutch = 0; //clutch switch, byte 52 bit 4
+uint8_t neutral = 0; //neutral switch, byte 53 bit 1
+
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
@@ -64,7 +79,7 @@ void setup() {
   timerSetup(10);
 
   //diag UART setup
-  pinMode(KLINE_PIN, OUTPUT);//setup to send init on k and l lines
+  pinMode(KLINE_PIN, OUTPUT);//Kline must be high for >50ms before init
   pinMode(LLINE_PIN, OUTPUT);
   digitalWrite(KLINE_PIN, HIGH);
   digitalWrite(LLINE_PIN, HIGH);
@@ -77,7 +92,6 @@ void setup() {
   display.clearDisplay();
   display.drawBitmap(32,0,logo_splash,64,64,SSD1306_WHITE);
   display.display();
-  delay(3000);
   display.clearDisplay();
   
 
@@ -119,10 +133,9 @@ void loop() {
         valid_cmd = 1;
         timer_ms = 0;
         writeFromProgmem(sensor_data, 7);
-        cmd_buf = F("Querying Sensors");
-        delay(500); //allow ECU time to respond
-        Serial1.readBytes(data_buf, Serial1.available()); //read ECU data
-        Serial.write(data_buf,64);
+        cmd_buf = F("Data requested");
+        readResponse();//get data returned by ECU
+        parseData(); //update stats and print data
       }
       //display output
       if(valid_cmd){
@@ -141,10 +154,56 @@ void loop() {
   }
 }
 
+//Message timings (2006 Vstrom DL650): 5-7ms arduino to send message, 32-33ms ECU to begin response, 54-55ms ECU to transmit sensor data for request 0x21 0x08, 5-7ms ECU to respond to keepalive
+void readResponse(){//read bytes returned by ECU
+  delay(10); //ensure that UART is done writing
+  while(Serial1.available()){ //because the UART is half-duplex, have to flush sent characters from the recieve buffer
+    Serial1.read();
+  }
+  delay(100);//wait for ECU to finish sending whole response
+  memset(data_buf, 0, 64);//clear data buffer
+  Serial1.readBytes(data_buf, Serial1.available());//read recieved bytes
+  if(data_buf[0]){//if data read, report success
+    cmd_buf = F("Data queried successfully");
+  }
+}
+
+void parseData(){ //parse received data and update variables
+  Serial.println(F("Recieved data: "));
+  for(uint8_t i = 0; i < 64; i++){//print all recieved data to USB serial
+    Serial.print(data_buf[i], HEX);
+    Serial.print(F(" "));
+  }
+  Serial.println("\n");
+  //update and print stats
+  //speed
+  speed = data_buf[16] * 2;
+  Serial.print(F("Speed KPH: "));
+  Serial.println(speed, DEC);
+  //rpm
+  rpm = (data_buf[17] * 10) + (data_buf[18] / 10);
+  Serial.print(F("RPM: "));
+  Serial.println(rpm, DEC);
+  //throttle position sensor
+  tps = (data_buf[19] - 0x37)/0xA6;
+  Serial.print(F("TPS %: "));
+  Serial.println(tps, DEC);
+  //
+}
+
+uint8_t iap1 = 0; //intake air pressure 1, byte 20
+uint8_t engine_temp = 0; //engine oil temperature in celsius, (byte 21-48)/1.6
+uint8_t intake_temp = 0; //intake air temperature in celsius, (byte 22-48)/1.6
+uint8_t v_bat = 0; //battery voltage, byte 24*20/255
+uint8_t gear = 0; //gear, byte 26
+uint8_t iap2 = 0; //intake air pressure 2, byte 27
+uint8_t throt_valv = 0; //throttle valve, byte 46
+uint8_t clutch = 0; //clutch switch, byte 52 bit 4
+uint8_t neutral = 0; //neutral switch, byte 53 bit 1
 
 void fastInit(){ //fast KWP2000 init protocol on k and l lines
   initialized = 0;//stop keepalives if retrying intialization
-  Serial1.end();
+  Serial1.end(); //release serial for GPIO control
   delay(100); //KWP2000 init protocol: >50ms high, 25ms low, 25ms high, send init sequence
   digitalWrite(KLINE_PIN, LOW);
   digitalWrite(LLINE_PIN, LOW);
@@ -158,7 +217,7 @@ void fastInit(){ //fast KWP2000 init protocol on k and l lines
   initialized = 1;//begin sending keepalives
 }
 
-void writeFromProgmem(char* msg, uint8_t msglen){
+void writeFromProgmem(char* msg, uint8_t msglen){//allows saving messages in flash
   uint8_t count = 0;
   while(count < msglen){
     write_buf[count] = pgm_read_byte_near(msg + count);
